aa572a81f329961679cbdf7f082bef49c1404051
iOnly <57323123+phumjaisim@users.noreply.github.com>
2021-04-22 04:22:17 +0700

    aCommands V2021.1-b2.0.2003
    aCommands Version 1.2.0.2003 beta for aMenu V3
    
    aCommands Version 2021.1-b2.0.2003
    
    Added:
    
    New Database connection now only open gateway
    
    - /autils-info
              display aUtils version currently running on
    - /bemonke
              transform yourself to be monkey

--- aCommands/src/main/java/com/ionly/acommands/aCommands.java
+++ aCommands/src/main/java/com/ionly/acommands/aCommands.java
@@ -1,5 +1,6 @@
 package com.ionly.acommands;
 
+import com.ionly.acommands.sql.MySQL;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -7,20 +8,41 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.SQLException;
+
 
 public final class aCommands extends JavaPlugin {
 
+    public MySQL SQL;
+
     @Override
     public void onEnable() {
-        System.out.println("[aCommands] aCommand V2.0 by iOnly");
+        System.out.println("[aCommands] aCommand V2021.1-b2.0.2003");
         System.out.println("[aCommands] aCommands has been operated");
         System.out.println("[aCommands] attempting to connecting to Dashboard database.");
+        this.SQL = new MySQL();
+
+        try {
+            SQL.connect();
+        } catch (ClassNotFoundException e) {
+            //e.printStackTrace();
+            Bukkit.getLogger().info("Database connecting abort! [!ERROR!]");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        if (SQL.isConnected()) {
+            Bukkit.getLogger().info("Gateway Opened! [aCommands > aUtils Database]");
+            Bukkit.getLogger().info("Catching and preparing to connect to the aUtils Database");
+            Bukkit.getLogger().info("the aUtils Database is connected with aCommands");
+        }
     }
 
     @Override
     public void onDisable() {
         System.out.println("[aCommands] attempting to disconnecting from Dashboard database.");
         System.out.println("Database was disconnected!");
+        SQL.disconnect();
     }
 
     @Override
@@ -28,6 +50,7 @@ public final class aCommands extends JavaPlugin {
         if (command.getName().equals("amenubruh")){
             if(sender instanceof Player){
                 Player player = (Player) sender;
+                player.sendMessage(ChatColor.AQUA + "You just bruh to the whole server");
                 Bukkit.broadcastMessage(ChatColor.GREEN + "[" + player.getDisplayName() +"]" + " was bruh to the whole server.");
             }else{
                 System.out.println("Need to be player to use this command");
@@ -37,7 +60,19 @@ public final class aCommands extends JavaPlugin {
         if (command.getName().equals("autils-info")){
             if(sender instanceof Player){
                 Player player = (Player) sender;
-                player.sendMessage("aMenu V");
+                player.sendMessage("aMenu V3.0.2");
+                player.sendMessage("aCommands V2021.1-b2.0.2003");
+                player.sendMessage("aDashBoard V2021.1-db1.1 | https://adashboard.ga");
+            }else{
+                System.out.println("aMenu V3.0.2 - aCommands V2021.1-b2.0.2003 - aDashBoard V2021.1-db1.1");
+            }
+        }
+
+        if (command.getName().equals("bemonke")){
+            if(sender instanceof Player){
+                Player player = (Player) sender;
+                player.sendMessage(ChatColor.GOLD + "u just transform urself the MONKE!!");
+                player.setDisplayName(ChatColor.GOLD + "MONKE_" + player.getName());
             }else{
                 System.out.println("Need to be player to use this command");
             }
---
+++ aCommands/src/main/java/com/ionly/acommands/sql/MySQL.java
@@ -0,0 +1,43 @@
+package com.ionly.acommands.sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQL {
+
+    private String host = "4.182.76.206";
+    private String port = "3306";
+    private String database = "adashboard_ingamedb";
+    private String username = "adashboardadmin";
+    private String password = "adashboard.ga";
+
+    private Connection connection;
+
+    public boolean isConnected() {
+        return (connection == null ? false : true);
+    }
+
+    public void connect() throws ClassNotFoundException, SQLException {
+        if (!isConnected()) {
+            connection = DriverManager.getConnection("jdbc:mysql://" +
+                            host + ":" + port + "/" + database + "?useSSL=false",
+                    username, password);
+        }
+    }
+
+    public void disconnect() {
+        if (isConnected()) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+}
